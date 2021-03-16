5c886c9fca234b400b8f290e44a3f364ec435dc1
advistasyam <advis.tasyah@ui.ac.id>
2021-03-16 13:03:30 +0700

    w02 progress

---
+++ TXT/myscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright (C) 2020-2021 Cicak Bin Kadal
+# https://www.youtube.com/watch?v=KAXK07ni9gU
+
+FILES="my.txt my.sh"
+SHA="SHA256SUM"
+
+echo "rm -f $SHA $SHA.asc"
+rm -f $SHA $SHA.asc
+
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo "gpg -o $SHA.asc -a -sb $SHA"
+gpg -o $SHA.asc -a -sb $SHA
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
+
+exit 0
